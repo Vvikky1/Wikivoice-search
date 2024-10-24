cd5654f17aa3f3a910ad047df97646736f54925b
Vvikky1 <vikramvikky4098@gmail.com>
2024-10-24 19:12:10 +0530

    Initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vikky\Desktop\chat\v\venv
+set VIRTUAL_ENV=C:\Users\vikky\Desktop\chat\QyLoud\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
