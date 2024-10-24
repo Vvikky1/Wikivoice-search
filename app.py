1b9818f465ed859b71d21b2444b51f654f0f0254
Vvikky1 <128999965+Vvikky1@users.noreply.github.com>
2024-10-24 19:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_audio():
